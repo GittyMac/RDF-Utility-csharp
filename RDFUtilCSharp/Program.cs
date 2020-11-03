8df5c56f77713f72454d54725ff9be60d15380e3
Lako <Lako>
2020-11-03 17:22:48 -0500

    Add project files.

---
+++ RDFUtilCSharp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RDFUtilCSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//
+			//Init
+			//
+			RDFTool RDFTool = new RDFTool();
+			string FileLoc = args[0];
+			Encoding iso_8859_1 = Encoding.GetEncoding("iso-8859-1");
+			byte[] RDFData = File.ReadAllBytes(FileLoc);
+			string RDFConverted;
+
+			//
+			//Intro
+			//
+			Console.WriteLine("RDF Conversion Utility (C#)");
+			Console.WriteLine("Original Program by WeNeedCoffee");
+			Console.WriteLine("Ported by Lako");
+
+			//
+			//Conversion
+			//
+			if (File.Exists(FileLoc))
+			{
+				if (FileLoc.Contains(".rdf"))
+				{
+					RDFConverted = RDFTool.decode(iso_8859_1.GetString(RDFData));
+					File.WriteAllText(FileLoc.Replace(".rdf", ".xml"), RDFConverted);
+					Console.WriteLine("RDF successfully converted!");
+				}
+				else if (FileLoc.Contains(".xml"))
+				{
+					RDFConverted = RDFTool.encode(iso_8859_1.GetString(RDFData));
+					File.WriteAllBytes(FileLoc.Replace(".xml", ".rdf"), iso_8859_1.GetBytes(RDFConverted));
+					Console.WriteLine("XML successfully converted!");
+				}
+			}
+			else Console.WriteLine("File does not exist!");
+		}
+
+
+	}
+}
---
+++ RDFUtilCSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RDF Conversion Utility")]
+[assembly: AssemblyDescription("A tool to convert Radica Data Files.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lako")]
+[assembly: AssemblyProduct("RDF Conversion Utility")]
+[assembly: AssemblyCopyright("Copyright © Lako 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1dc4755-0b47-414e-8c02-d9626c1a1c2f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
